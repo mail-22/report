c1967a82da7f7b54d7ab50a08accadb1f52c5756
Toshiba <mail-22@ya.ru>
2018-01-25 23:59:01 +0300

    1

---
+++ source/JvDSADialogs/MessageDlgEditor.dpr
@@ -0,0 +1,16 @@
+program MessageDlgEditor;
+
+uses
+  ExceptionLog,
+  Forms,
+  MessageDlgEditorMain in 'MessageDlgEditorMain.pas' {frmMessageDlgEditor},
+  MessageDlgEditorSelectIcon in 'MessageDlgEditorSelectIcon.pas' {frmMessageDlgEditorSelectIcon};
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.Title := '(DSA)MessageDlg(Ex) editor';
+  Application.CreateForm(TfrmMessageDlgEditor, frmMessageDlgEditor);
+  Application.Run;
+end.

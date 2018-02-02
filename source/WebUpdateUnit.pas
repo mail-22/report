701c990ce51834b1e0cf68daa604e3489ee26a95
Toshiba <mail-22@ya.ru>
2018-02-02 17:27:56 +0300

    ver.2

--- source/WebUpdateUnit.pas
+++ source/WebUpdateUnit.pas
@@ -36,18 +36,19 @@ begin
      ShowMessage('A new version of this application is available !');
      WebUpdate1.DoUpdate(true);
    end;
-}                         
+}
    WebUpdate1.LogFileName := ExtractFilePath(Application.ExeName)+'\'+ 'WUPDATE.LOG';
+{
    WebUpdate1.Connected;
    WebUpdate1.StartConnection;
-   WebUpdate1.GetControlFile;   // ???
+ //WebUpdate1.GetControlFile;   // ???
    //WebUpdate1.DoVersionCheck;
    if WebUpdate1.NewVersionInfo > WebUpdate1.CurVersionInfo
    then WebUpdate1.DoUpdate;;
    //WebUpdate1.s
    WebUpdate1.StopConnection;
    //WebUpdate1.DoUpdate;
-
+}
    WebUpdateWizard1.Execute(true);
    //WebUpdateForm.close;
 end;
